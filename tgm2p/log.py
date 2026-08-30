e1f3642f194bfdf723616597cd740a82e71c688a
HWXLR8 <68k@segv.moe>
2026-08-30 15:19:01 -0400

    flush to disk after each write

--- tgm2p/log.py
+++ tgm2p/log.py
@@ -1,11 +1,14 @@
 #!/usr/bin/env python3
 
 import datetime
+import os
 
 f = open('log.csv', 'a')
 f.write(datetime.datetime.today().strftime("%Y-%m-%d"))
 
 while line := input("enter level: "):
     f.write(',' + line)
+    f.flush()
+    os.fsync(f.fileno())
 
 f.write('\n')
